3e5b291a9680a016864ec916c9d568e55ea654fe
siyue.huang <siyue.huang@medbanks.cn>
2021-04-27 13:53:31 +0800

    feat: add docs

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  outputPath: 'docs',
+  publicPath: './',
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
 });
