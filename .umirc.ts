621a57b94d4711f555ef7226705c06f4c8856a6c
siyue.huang <siyue.huang@medbanks.cn>
2021-04-27 14:09:06 +0800

    upd: update config

--- .umirc.ts
+++ .umirc.ts
@@ -3,8 +3,15 @@ import { defineConfig } from 'umi';
 export default defineConfig({
   outputPath: 'docs',
   publicPath: './',
+  base: '/irregular-note',
   nodeModulesTransform: {
     type: 'none',
+  },
+  hash: true,
+  title: false,
+  ignoreMomentLocale: true,
+  alias: {
+    '@': require('path').resolve(__dirname, 'src'),
   },
   routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
