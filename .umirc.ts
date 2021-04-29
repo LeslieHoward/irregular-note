0ffe50fe16ec3971ba04fc534773bd986ca7ece6
siyue.huang <siyue.huang@medbanks.cn>
2021-04-29 18:48:19 +0800

    upd: 调整项目配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,32 @@
 import { defineConfig } from 'dumi';
 
+const path = require('path');
+const fs = require('fs');
+const _ = require('lodash');
+
+function generateRoutes(targetPath) {
+  let files = fs.readdirSync(targetPath);
+  return _.reduce(
+    files,
+    (acc, item) => {
+      if (/\d+[^\d]\.md$/.test(item)) {
+        acc['/2021'].push({ title: item, path: `${item}` });
+      }
+      return acc;
+    },
+    { '/2021': [] },
+  );
+}
+
 export default defineConfig({
   base: '/irregular-note/',
   publicPath: '/irregular-note/',
-  title: '周报',
   mode: 'site',
   dynamicImport: {},
+  title: '兑刊',
+  logo: '/images/logo.svg',
+  // navs: [null],
+  theme: {
+    '@primary-color': '#a0d911',
+  },
 });
